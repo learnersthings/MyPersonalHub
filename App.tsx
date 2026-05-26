c2b4e46eaa2f12b3992e226e7faa6b6ae64a14d6
learnersthings <lt.learnersthings@gmail.com>
2026-05-26 09:31:17 +0530

    Created an app.

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>My Personal Hub</Text>
+    </SafeAreaView>
+  );
+}
